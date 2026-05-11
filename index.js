1c0c67ea32a5eaaed134406d5f512e33a2b72414
Andreas Ioannou <aioannou@Michaels-MacBook-Pro.local>
2026-05-11 09:32:04 +0300

    pushing final product before deployment

--- index.js
+++ index.js
@@ -22,12 +22,9 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-const db = new pg.Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+const db  = new pg.Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 });
 db.connect();
 
